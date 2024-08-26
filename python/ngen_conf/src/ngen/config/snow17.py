d2d29bfd15bea7823c96dec715517634466c5166
Mohammed Karim <mohammed.r.karim@rtx.com>
2024-08-26 15:05:36 -0700

    update

--- python/ngen_conf/src/ngen/config/snow17.py
+++ python/ngen_conf/src/ngen/config/snow17.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 from pydantic import BaseModel, Field
 
 from .bmi_formulation import BMIFortran
@@ -8,8 +8,13 @@ class Snow17Params(BaseModel):
     """Class for validating snow17 Parameters
     """
     #define params which can be adjusted here
-    #see cfe.py for example
-    pass
+    #see cfe.py for example4
+    mfmin: Optional[float]
+    mfmax: Optional[float]
+    udaj: Optional[float]
+    pxtemp: Optional[float]
+    timp: Optional[float]
+    
 
 class Snow17(BMIFortran):
     """A BMIFortran implementation for a snow17 module
@@ -18,10 +23,9 @@ class Snow17(BMIFortran):
     model_params: Snow17Params = None
     main_output_variable: str = 'raim'
     #NOTE aliases don't propagate to subclasses, so we have to repeat the alias
-    model_name: Literal["snow17"] = Field("Snow17", const=True, alias="model_type_name")
+    model_name: Literal["snow17"] = Field("snow17", const=True, alias="model_type_name")
 
     _variable_names_map =  {
             "precip": "atmosphere_water__liquid_equivalent_precipitation_rate",
-            "tair": "land_surface_air__temperature",
-            "raim": "atmosphere_water__liquid_equivalent_precipitation_rate"
+            "tair": "land_surface_air__temperature"
         }
