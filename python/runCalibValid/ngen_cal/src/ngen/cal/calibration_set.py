d4b426fe579122531ed96f705938f0b7d76dc4c6
peter.a.kronenberg <peter.a.kronenberg@rtx.com>
2025-01-23 17:18:56 -0500

    Fix message

--- python/runCalibValid/ngen_cal/src/ngen/cal/calibration_set.py
+++ python/runCalibValid/ngen_cal/src/ngen/cal/calibration_set.py
@@ -127,7 +127,7 @@ class CalibrationSet(Evaluatable):
             hydrograph = self._output
 
         except FileNotFoundError:
-            logger.info("Preparing for initial simulation.  Hydrograph output is currently empty.")
+            logger.info("Output is currently empty.")
             hydrograph = None
         except Exception as e:
             raise(e)
