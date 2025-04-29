24713446299cf928734244b6395eb7f28ca444db
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-04-29 07:55:44 -0700

    Change ENABLE to ENABLED for EWTS flag

--- python/runCalibValid/calibration.py
+++ python/runCalibValid/calibration.py
@@ -133,7 +133,7 @@ def main(general: General, model_conf):
     for i in range(len(log_level_array)):
         logging.info(f"\t{log_level_array[i,0]} = {log_level_array[i,1]}")
         os.environ[log_level_array[i,0]] = log_level_array[i,1]
-    ewts_logging = 'ENABLE'
+    ewts_logging = 'ENABLED'
     os.environ['NGEN_EWTS_LOGGING'] = ewts_logging
     logging.info(f"NGEN_EWTS_LOGGING = {ewts_logging}")
 
