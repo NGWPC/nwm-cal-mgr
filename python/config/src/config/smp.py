44bb58ff5ffa6593571d48dbcf46072717be55e8
yuqiong.liu <yuqiong.liu@ertcorp.com>
2025-08-13 15:32:53 -0400

    formatting and package name changes

---
+++ python/config/src/config/smp.py
@@ -0,0 +1,18 @@
+from typing import Mapping, Optional
+
+from pydantic import BaseModel, Field
+
+from .bmi_formulation import BMICxx
+
+
+class SMP(BMICxx):
+    """A BMIC++ implementation for SMP module"""
+
+    registration_function: str = "none"
+    main_output_variable: str = "soil_water_table"
+    model_name: str = Field("SMP", const=True, alias="model_type_name")
+
+    _variable_names_map = {
+        "soil_storage": "SOIL_STORAGE",
+        "soil_storage_change": "SOIL_STORAGE_CHANGE",
+    }
